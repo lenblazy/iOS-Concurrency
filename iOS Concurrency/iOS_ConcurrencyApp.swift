a75c462fe9960825c6ddd333d508e8b65fd50062
lenblazy <lenblazy@gmail.com>
2023-06-15 11:31:00 +0300

    Initial Commit

---
+++ iOS Concurrency/iOS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ConcurrencyApp.swift
+//  iOS Concurrency
+//
+//  Created by Lennox Mwabonje on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
